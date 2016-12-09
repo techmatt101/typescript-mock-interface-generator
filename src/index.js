528ecfee2c9debef666b3f7ba42380b871deb79f
techmatt101 <matt@kempmail.co.uk>
2016-12-09 04:02:26 +0000

    now maintains module namespace for interfaces interfaces nested inside modules will now maintain in the same namespace when being generated

--- src/index.js
+++ src/index.js
@@ -4,33 +4,60 @@ const ts = require("typescript");
 class TsMockInterfaceGenerator {
 
     constructor() {
-        this._interfaces = [];
+        this._modules = {
+            node: null,
+            modules: [],
+            interfaces: []
+        };
     }
 
     add(content) {
         ts.createSourceFile('', content.toString(), ts.ScriptTarget.ES6, true)
             .getChildren()
-            .forEach(node => this._addNestedInterfaces(node));
+            .forEach(node => this._addNestedInterfaces(node, this._modules));
     }
 
-    _addNestedInterfaces(node) {
+    _addNestedInterfaces(node, module) {
         if (node.kind === ts.SyntaxKind.InterfaceDeclaration) {
-            this._interfaces.push(node);
+            module.interfaces.push(node);
         }
-        else if (node.kind === ts.SyntaxKind.SyntaxList) {
-            node.getChildren().forEach(n => this._addNestedInterfaces(n));
+        else if (node.kind === ts.SyntaxKind.ModuleDeclaration) {
+            var nestedModule = module.modules.find(x => x.node.name.text === node.name.text);
+            if(!nestedModule) {
+                nestedModule = {
+                    node: node,
+                    modules: [],
+                    interfaces: []
+                };
+                module.modules.push(nestedModule);
+            }
+            node.getChildren().forEach(n => this._addNestedInterfaces(n, nestedModule));
+        }
+        else if (node.kind === ts.SyntaxKind.SyntaxList || node.kind === ts.SyntaxKind.ModuleBlock) {
+            node.getChildren().forEach(n => this._addNestedInterfaces(n, module));
         }
     }
 
-    generate() {
-        return this._interfaces.map(interfaceNode => {
+    _generateOutput(module) {
+        var output = [];
+
+        module.interfaces.forEach(interfaceNode => {
             var properties = interfaceNode.members
                 .filter(x => x.kind === ts.SyntaxKind.PropertySignature || x.kind === ts.SyntaxKind.PropertyDeclaration)
                 .map((member) => `${member.name.text} = ${TsMockInterfaceGenerator.generateValueFromInterfaceMember(member)};`);
 
-            return `class ${interfaceNode.name.text} { ${properties.join(' ')} }`;
-        })
-        .join('\n');
+            output.push(`class ${interfaceNode.name.text} { ${properties.join(' ')} }`);
+        });
+
+        module.modules.forEach(childModule => {
+            output.push(`module ${childModule.node.name.text} { ${this._generateOutput(childModule).map(x => 'export ' + x).join(' ')} }`);
+        });
+
+        return output;
+    }
+
+    generate() {
+        return this._generateOutput(this._modules).join('\n');
     }
 
     static generateValueFromInterfaceMember(member) {
--- test/test.spec.js
+++ test/test.spec.js
@@ -4,7 +4,32 @@ const TsMockInterfaceGenerator = require('../src/index');
 describe('Tests', () => {
     it('interface', () => {
         var subject = new TsMockInterfaceGenerator();
-        subject.add('interface A {}');
-        assert.equal(subject.generate(), 'class A {  }');
+        subject.add('interface A { test: string; }');
+        assert.equal(subject.generate(), 'class A { test = new String(); }');
+    });
+
+    it('module', () => {
+        var subject = new TsMockInterfaceGenerator();
+        subject.add('module A { interface A {} }');
+        assert.equal(subject.generate(), 'module A { export class A {  } }');
+    });
+
+    it('export module', () => {
+        var subject = new TsMockInterfaceGenerator();
+        subject.add('export module A { interface A {} }');
+        assert.equal(subject.generate(), 'module A { export class A {  } }');
+    });
+
+    it('multiple modules', () => {
+        var subject = new TsMockInterfaceGenerator();
+        subject.add('module A { interface A {} }');
+        subject.add('module A { interface B {} }');
+        assert.equal(subject.generate(), 'module A { export class A {  } export class B {  } }');
+    });
+
+    it('nested modules', () => {
+        var subject = new TsMockInterfaceGenerator();
+        subject.add('module A { module B { interface A {} } }');
+        assert.equal(subject.generate(), 'module A { export module B { export class A {  } } }');
     });
 });
